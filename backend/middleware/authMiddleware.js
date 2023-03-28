99f5e51ea1a20846452fddc30310a69f25570546
Harsh <harshgupta3341@gmail.com>
2023-03-28 18:28:44 +0530

    Add authMiddleware and jwtToken files

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const Jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  const { token } = req.cookies;
+  {
+    try {
+      const decoded = Jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  // check if token exists
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = protect;
